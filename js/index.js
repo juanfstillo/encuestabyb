83a878ed03b7b67587043ed05afafb99c5092488
Juan Stillo <juan.stillo@trenesargentinos.gob.ar>
2023-05-11 14:36:29 -0300

    varios cambios

--- js/index.js
+++ js/index.js
@@ -1,16 +1,45 @@
-var randomlinks=new Array(10)
+let acepto = document.getElementById("acepto");
 
- randomlinks[0]="https://www.mercadolibre.com.ar/"
- randomlinks[1]="https://www.ebay.com/"
- randomlinks[2]="https://www.amazon.com/"
- randomlinks[3]="https://www.despegar.com.ar/"
- randomlinks[4]="https://www.youtube.com/"
- randomlinks[5]="https://www.netflix.com/ar"
- randomlinks[6]="https://www.infobae.com/"
- randomlinks[7]="https://www.promiedos.com.ar/"
- randomlinks[8]="https://open.spotify.com/"
- randomlinks[9]="https://getbootstrap.com/"
- randomlinks[10]="https://www.php.net/"
+acepto.addEventListener("click", function() {
+  if (acepto.checked) {
+   document.getElementById('boton').classList.remove('disabled');
+} else {
+   document.getElementById('boton').classList.add('disabled');
+  }
+});
+
+var randomlinks=new Array(29)
+
+ randomlinks[0]="https://ugx2zv1j3sq.typeform.com/to/n2jthYmv#aehj=xjh6"
+ randomlinks[1]="https://ugx2zv1j3sq.typeform.com/to/bbYKx3Qm#aehj=xjh6"
+ randomlinks[2]="https://ugx2zv1j3sq.typeform.com/to/yJYPmL5y#aehj=xjh6"
+ randomlinks[3]="https://ugx2zv1j3sq.typeform.com/to/iAd74D2a#aehj=xjh6"
+ randomlinks[4]="https://ugx2zv1j3sq.typeform.com/to/MvSuF7Sh#aehj=xjh6"
+ randomlinks[5]="https://ugx2zv1j3sq.typeform.com/to/obh684NQ#aehj=xjh6"
+ randomlinks[6]="https://ugx2zv1j3sq.typeform.com/to/e5hC2Wfn#aehj=xjh6"
+ randomlinks[7]="https://ugx2zv1j3sq.typeform.com/to/isR0pAHx#aehj=xjh6"
+ randomlinks[8]="https://ugx2zv1j3sq.typeform.com/to/ieFXDSh6#aehj=xjh6"
+ randomlinks[9]="https://ugx2zv1j3sq.typeform.com/to/Vk2fnAHH#aehj=xjh6"
+ randomlinks[10]="https://ugx2zv1j3sq.typeform.com/to/jg1BXWwc#aehj=xjh6"
+ randomlinks[11]="https://ugx2zv1j3sq.typeform.com/to/KCiUGXqM#aehj=xjh6"
+ randomlinks[12]="https://ugx2zv1j3sq.typeform.com/to/dN4Mr7Pv#aehj=xjh6"
+ randomlinks[13]="https://ugx2zv1j3sq.typeform.com/to/qxGveRmj#aehj=xjh6"
+ randomlinks[14]="https://ugx2zv1j3sq.typeform.com/to/Ano44fFQ#aehj=xjh6"
+ randomlinks[15]="https://ugx2zv1j3sq.typeform.com/to/T0sSF3gr#aehj=xjh6"
+ randomlinks[16]="https://ugx2zv1j3sq.typeform.com/to/rsptXZ2U#aehj=xjh6"
+ randomlinks[17]="https://ugx2zv1j3sq.typeform.com/to/Ob0fI3MN#aehj=xjh6"
+ randomlinks[18]="https://ugx2zv1j3sq.typeform.com/to/uQe9yoFW#aehj=xjh6"
+ randomlinks[19]="https://ugx2zv1j3sq.typeform.com/to/i2ws3D7B#aehj=xjh6"
+ randomlinks[20]="https://ugx2zv1j3sq.typeform.com/to/Lsj4y1zz#aehj=xjh6"
+ randomlinks[21]="https://ugx2zv1j3sq.typeform.com/to/c1IJhuJS#aehj=xjh6"
+ randomlinks[22]="https://ugx2zv1j3sq.typeform.com/to/T3nTyqy9#aehj=xjh6"
+ randomlinks[23]="https://ugx2zv1j3sq.typeform.com/to/YWk5C0lJ#aehj=xjh6"
+ randomlinks[24]="https://ugx2zv1j3sq.typeform.com/to/P7chNQva#aehj=xjh6"
+ randomlinks[25]="https://ugx2zv1j3sq.typeform.com/to/iQkxqZbC#aehj=xjh6"
+ randomlinks[26]="https://ugx2zv1j3sq.typeform.com/to/QelYRfLy#aehj=xjh6"
+ randomlinks[27]="https://ugx2zv1j3sq.typeform.com/to/g7L436Pq#aehj=xjh6"
+ randomlinks[28]="https://ugx2zv1j3sq.typeform.com/to/GQBmxQq0#aehj=xjh6"
+ randomlinks[29]="https://ugx2zv1j3sq.typeform.com/to/o554ABEa#aehj=xjh6"
 
  function randomlink(){
     window.location=randomlinks[Math.floor(Math.random()*randomlinks.length)]
